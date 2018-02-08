619dd44cac211ba5ac7789b8c1de6469e80d68ee
paolozroxas <paolo.z.roxas@gmail.com>
2018-02-07 16:05:17 -0800

    Served static files.

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -11,6 +11,27 @@ exports.headers = {
 };
 
 exports.serveAssets = function(res, asset, callback) {
+  if (fs.statSync(asset).isDirectory()) {
+    asset += 'index.html';
+  }
+    
+  fs.exists(asset, (exist)=>{
+    
+    if (!exist) {
+      res.writeHead(404, 'File Not Found', this.headers); //Error 404 - File Not Found
+    } 
+
+    fs.readFile(asset, (error, data)=>{
+      if (error) {
+        //Error 500 - Internal Server Error
+      } else {
+        callback(data);
+      }
+    });
+    
+  });
+  
+  
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...),
   // css, or anything that doesn't change often.)
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,33 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
+var httpHelpers = require('./http-helpers');
+var url = require('url');
+
+var methods = {
+  GET: (req, res) => {
+    console.log('GET Request Received!');
+    var parsedUrl = url.parse(req.url);
+    var pathName = `./web/public${parsedUrl.pathname}`;
+    console.log('pathname = ' + pathName);
+    httpHelpers.serveAssets(res, pathName, (data)=>{
+      res.writeHead(200, 'Okay!', httpHelpers.headers);
+      res.end(data);
+    });
+  },
+  POST: (req, res)=>{
+    
+  },
+  OPTIONS: (req, res)=>{
+    
+  }
+};
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var method = methods[req.method];
+  if (method) {
+    method(req, res);
+  } else {
+    //402 Error - Invalide Method
+  }
 };
